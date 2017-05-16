a2753212e8d12143260c0a37e6115edd83bae1d5
NgoGiaLoc96 <ngogialoc96@gmail.com>
2017-05-16 22:32:50 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('templates.home');
+// });
+
+// Access from url
+Route::get('/', 'PageController@home');
+Route::get('/category', 'PageController@category');
+Route::get('/category/{id_type}', ['as' => 'question', 'uses' =>'PageController@questionType1']);
+Route::get('/user', 'PageController@user');
+Route::get('/addQuestion', 'PageController@addQuestion');
+
+// Route::get('/homepage', function() {
+// 	return view('templates.home');
+// });
+
+// Route::get('/list', function() {
+// 	return view('templates.list');
+// });
+
+// Route::get('/q1', function() {
+// 	return view('templates.questionType1');
+// });
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index');
+
+// Question
+Route::post('/addQuestion/add', 'QuestionController@add');
+Route::post('delete/{id_type}/{id_question}', [ 'as' => 'delete', 'uses' => 'QuestionController@delete']);
+Route::get('/questionList/{id_type}', [ 'as' => 'questionList', 'uses' => 'QuestionController@questionList']);
+
+//User Management
+Route::get('setAdmin/{id_user}',[ 'as' => 'setAdmin', 'uses' =>'UserController@setAdmin' ]);
+Route::get('setQuestionaire/{id_user}',[ 'as' => 'setQuestionaire', 'uses' =>'UserController@setQuestionaire' ]);
+Route::get('deleteUser/{id_user}',[ 'as' => 'deleteUser', 'uses' =>'UserController@deleteUser' ]);
+
+
